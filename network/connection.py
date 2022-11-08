fedfdd1e6989579f2d44be81cb910649202489a0
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2022-11-07 23:18:23 -0500

    Abstract Connection Details
    Create Connection dataclass to encapsulate connection details and reduce the number of passed arguments.

---
+++ network/connection.py
@@ -0,0 +1,15 @@
+from socket import socket
+from typing import TypeVar, TypedDict
+
+Address = tuple[str, int]
+IncomingConnection = tuple[socket, Address]
+
+
+class ClientConnection:
+  """Represents a server-client connection with client details. """
+  client: socket
+  ip: str
+  port: int
+
+  def __init__(self, connection: IncomingConnection) -> None:
+    self.client, (self.ip, self.port) = connection
--- network/server.py
+++ network/server.py
@@ -3,9 +3,10 @@ from threading import Thread
 from threading import active_count as active_threads
 from typing import TYPE_CHECKING, Any, NoReturn
 from network.config import Config, ServerConfig
+from network.connection import ClientConnection, IncomingConnection
 
 from network.device import Device
-from chat.message_types import MessageType
+from chat.message import MessageType
 
 if TYPE_CHECKING:
   from socket import socket
@@ -37,42 +38,41 @@ class ChatServer(Device):
   def await_connections(self) -> NoReturn:
     """Waits for incoming connections and starts a thread for each connection."""
     while True:
-      connection, (ip, port) = self.server.accept()
-      thread: Thread = Thread(target=self.await_messages,
-                              args=(connection, ip, port))
+      incoming_connection: IncomingConnection = self.server.accept()
+      connection: ClientConnection = ClientConnection(incoming_connection)
+      thread: Thread = Thread(target=self.await_messages, args=(connection,))
       thread.start()
       print(f"[ACTIVE CONNECTIONS] {active_threads() - 1}")
 
-  def await_messages(self, client: socket, ip: str, port: int) -> None:
+  def await_messages(self, connection: ClientConnection) -> None:
     """Listens for client messages and sends responses."""
     connected: bool = True
     while connected:
-      message: dict[str, str] = self.receive_message(client)
-      connected = self.send_response(client, ip, port, message)
+      message: dict[str, str] = self.receive_message(connection.client)
+      connected = self.send_response(connection, message)
 
-  def send_response(self, client: socket, ip: str, port: int,
+  def send_response(self, connection: ClientConnection,
                     message: dict[str, str]) -> bool:
     """Sends appropriate response to the client based on the user's message."""
     if message["type"] == MessageType.DISCONNECT:
-      self.disconnect_user(client, ip, port)
+      self.disconnect_user(connection)
       return False
 
     chat_id: str = message["chat_id"]
     if message["type"] == MessageType.CONNECT:
-      self.assign_chatroom(client, chat_id, ip, port)
+      self.assign_chatroom(connection, chat_id)
 
     elif message["type"] == MessageType.MESSAGE:
       self.forward_message(message, chat_id)
 
     return True
 
-  def assign_chatroom(self, client: socket, id: str, ip: str,
-                      port: int) -> None:
+  def assign_chatroom(self, connection: ClientConnection, id: str) -> None:
     """Assigns user to a chatroom with the provided chat ID."""
 
-    print(f"[{ip}:{port}] Connected")
+    print(f"[{connection.ip}:{connection.port}] Connected")
     self.chats[id] = self.chats.get(id, [])
-    self.chats[id].append(client)
+    self.chats[id].append(connection.client)
 
     status: str = "Joined chatroom." if id in self.chats else "Created chatroom"
 
@@ -83,12 +83,12 @@ class ChatServer(Device):
         "message": status,
         "total_online": active_threads() - 1,
     }
-    self.send_message(client, response)
+    self.send_message(connection.client, response)
 
-  def disconnect_user(self, client: socket, ip: str, port: int) -> None:
+  def disconnect_user(self, connection: ClientConnection) -> None:
     """Closes the connection to disconnect user from chat."""
-    print(f"[{ip}:{port}] Disconnected")
-    client.close()
+    print(f"[{connection.ip}:{connection.port}] Disconnected")
+    connection.client.close()
 
   def forward_message(self, message: dict[str, str], chat_id: str) -> None:
     """Sends message to all chat particpants."""
